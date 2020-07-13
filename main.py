4a3610e2c5d8fbcc32f0b6ad502768555df4d00e
bowrango <mjb1085@wildcats.unh.edu>
2020-07-12 20:40:09 -0400

    getting cards in most used decks

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
-import urllib.request
-import time
 import cards
 from bs4 import BeautifulSoup
 
 url = 'https://statsroyale.com/decks/challenge-winners?type=grand&page=1'
-
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
+allDecks = soup.findAll("div", {"class": "recentWinners__decklist"})
+
+for deck in allDecks:
+    print('new deck')
+    cards = deck.findChildren('a', recursive=False)
+    for card in cards:
+        cardString = card.get('href')
+        print(type(cardString))
+    print('-------')
