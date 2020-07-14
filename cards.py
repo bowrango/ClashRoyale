e9e2e34664450db66a4bb4fbbfebfe6ea83eebb7
bowrango <mjb1085@wildcats.unh.edu>
2020-07-14 16:42:58 -0400

    minor improvements

--- cards.py
+++ cards.py
@@ -1,82 +1,32 @@
-class BabyDragon:
+class ThreeMusketeers:
 
-    flying = True
-    splash = True
+    def __init__(self, dmg, health, hitspeed, range, cost):
 
-    def __init__(self, dmg, health, hs, range, cost):
-
-        self.dmg = 113
-        self.health = 1024
-        self.hs = 1.5
-        self.range = 3.5
-        self.cost = 4
+        self.dmg = 0
+        self.health = 0
+        self.hitspeed = 0
+        self.range = 0
+        self.cost = 0
 
 
-class Wizard:
+class Golem:
 
-    flying = False
-    splash = True
+    def __init__(self, dmg, health, hitspeed, range, cost):
 
-    def __init__(self, dmg, health, hs, range, cost):
-
-        self.dmg = 234
-        self.health = 598
-        self.hs = 1.4
-        self.range = 5.5
-        self.cost = 5
+        self.dmg = 0
+        self.health = 0
+        self.hitspeed = 0
+        self.range = 0
+        self.cost = 0
 
 
-class MiniPekka:
+class RoyalRecruits:
 
-    flying = False
-    splash = False
+    def __init__(self, dmg, health, hitspeed, range, cost):
 
-    def __init__(self, dmg, health, hs, range, cost):
+        self.dmg = 0
+        self.health = 0
+        self.hitspeed = 0
+        self.range = 0
+        self.cost = 0
 
-        self.dmg = 598
-        self.health = 1129
-        self.hs = 1.8
-        self.range = 1
-        self.cost = 4
-
-
-class MagicArcher:
-
-    flying = False
-    splash = True
-
-    def __init__(self, dmg, health, hs, range, cost):
-
-        self.dmg = 111
-        self.health = 490
-        self.hs = 1.1
-        self.range = 7
-        self.cost = 4
-
-
-class MegaKnight:
-
-    flying = False
-    splash = True
-
-    def __init__(self, dmg, health, hs, range, cost):
-
-        self.dmg = 222
-        self.health = 3300
-        self.hs = 1.7
-        self.range = 1
-        self.cost = 7
-
-
-class Princess:
-
-    flying = False
-    splash = True
-
-    def __init__(self, dmg, health, hs, range, cost):
-
-        self.dmg = 140
-        self.health = 216
-        self.hs = 3
-        self.range = 9
-        self.cost = 3
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import requests
 import cards
+import re
+import pickle
+import os
 from bs4 import BeautifulSoup
 
-url = 'https://statsroyale.com/decks/challenge-winners?type=grand&page=1'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-allDecks = soup.findAll("div", {"class": "recentWinners__decklist"})
 
-for deck in allDecks:
-    print('new deck')
-    cards = deck.findChildren('a', recursive=False)
-    for card in cards:
-        cardString = card.get('href')
-        print(type(cardString))
-    print('-------')
+def get_decks(url):
+    """
+    :param url: page containing the most recent decks used by the winning top 200 players
+    :return: a list for each deck on the page containing the strings of the cards used
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    decksUsed = soup.findAll("div", {"class": "recentWinners__decklist"})
+
+    for deck in decksUsed:
+        allCards = deck.findChildren('a', recursive=False)
+
+        deckSet = []
+
+        for card in allCards:
+            # format card names
+            cardString = card.get('href').split('card/')[1]
+            cardString = cardString.replace('+', '').replace('-', '').replace('.', '')
+
+            deckSet.append(cardString)
+
+        print(deckSet)
+
+
+if __name__ == '__main__':
+
+    pagesToParse = 1
+    # URLs of grand challenge recent winners and top 200 players
+    grandURL = 'https://statsroyale.com/decks/challenge-winners?type=grand&page='
+    top200URL = 'https://statsroyale.com/decks/challenge-winners?type=top200&page='
+
+    for num in range(1, pagesToParse+1):
+        url = top200URL+str(num)
+        get_decks(url)
+
