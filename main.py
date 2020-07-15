07afea8775454d47b5b16efe5504df7b4f4bba4d
bowrango <mjb1085@wildcats.unh.edu>
2020-07-14 22:41:42 -0400

    mapping relations

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from bs4 import BeautifulSoup
 def get_decks(url):
     """
     :param url: page containing the most recent decks used by the winning top 200 players
-    :return: a list for each deck on the page containing the strings of the cards used
+    :return collection: a list of lists for each deck on the page containing the strings of the cards used
     """
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     decksUsed = soup.findAll("div", {"class": "recentWinners__decklist"})
+
+    collection = []
 
     for deck in decksUsed:
         allCards = deck.findChildren('a', recursive=False)
@@ -27,8 +29,27 @@ def get_decks(url):
 
             deckSet.append(cardString)
 
-        print(deckSet)
+        collection.append(deckSet)
 
+    return collection
+
+
+def map_cards(deck):
+    """
+    :param deck: a list of strings containing the cards used
+    :return: None
+    """
+    for card in deck:
+        if card not in cards:
+            cards[card] = {}
+
+        relatedCards = [c for c in deck if c != card]
+
+        for relatedCard in relatedCards:
+            cards[card][relatedCard] = 1
+
+
+cards = {}
 
 if __name__ == '__main__':
 
@@ -39,5 +60,6 @@ if __name__ == '__main__':
 
     for num in range(1, pagesToParse+1):
         url = top200URL+str(num)
-        get_decks(url)
+        for deck in get_decks(url):
+            map_cards(deck)
 
