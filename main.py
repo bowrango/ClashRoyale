47d249facd4c06232a7cf88166fb5e433e8b8337
bowrango <mjb1085@wildcats.unh.edu>
2020-07-26 20:20:08 -0400

    added normalize function

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import requests
-import cards
-import pickle
 from bs4 import BeautifulSoup
 
 
@@ -19,14 +17,18 @@ class Card:
         else:
             return None
 
+    def getMaxWeight(self):
+        return max(self.weights.values())
+
     def getLeastUsedWith(self):
         return min(self.weights, key=self.weights.get)
 
 
+# don't need now
 class Deck:
 
     # add some persistent variable here
-    def __init(self, player, trophies, result, opponent):
+    def __init__(self, player, trophies, result, opponent):
 
         self.player = player
         self.trophies = trophies
@@ -46,6 +48,7 @@ class Deck:
         return self.opponent
 
 
+# retrieve data
 def get_decks(url):
     """
     :param url: page containing HTMl deck data
@@ -74,43 +77,62 @@ def get_decks(url):
     return collection
 
 
-def map_cards(deck):
+def link_cards(deck):
     """
     :param deck: a list containing a string for each of the 8 cards
     :return: Nones
     """
     for card in deck:
-        if card not in cards:
-            # add a new Card instance to the dictionary
-            cards[card] = Card(card)
+        if card not in C:
+            # add the new Card instance to the dictionary
+            C[card] = Card(card)
 
         # relatedCards = deck.remove(card)
         relatedCards = [c for c in deck if c != card]
 
         for relatedCard in relatedCards:
 
-            # cards[card].weights is the property of the particular Card object we're looking at
-            # it is a dictionary containing the weights for each other card
-
-            if relatedCard not in cards[card].weights:
-                cards[card].weights[relatedCard] = 1
+            if relatedCard not in C[card].weights:
+                C[card].weights[relatedCard] = 1
             else:
-                cards[card].weights[relatedCard] += 1
+                C[card].weights[relatedCard] += 1
 
 
-cards = {}
-PlayerDecks = {}
+def normalize_weights(adjDict):
+    w = []
+    for card in adjDict:
+        w.append(adjDict[card].getMaxWeight())
 
+    normalizer = max(w)
+
+    return adjDict
+
+    # divide all values in adjDict by normalizer
+
+
+
+
+
+# StatsRoyale urls of grand challenge recent winners and top 200 players
+grandURL = 'https://statsroyale.com/decks/challenge-winners?type=grand&page='
+top200URL = 'https://statsroyale.com/decks/challenge-winners?type=top200&page='
+
+# RoyaleApi urls
+royaleURL = 'https://royaleapi.com/players/leaderboard'
+
+# The 'adjacency dictionary' containing the weights of the links between the cards.
+# C[card].weights is the property of the particular Card object we're looking at
+# it is a dictionary containing the weights for each other card
+C = {}
+
+# Fetch data from ladder
 if __name__ == '__main__':
 
     pagesToParse = 1
-    # Urls of grand challenge recent winners and top 200 players
-    grandURL = 'https://statsroyale.com/decks/challenge-winners?type=grand&page='
-    top200URL = 'https://statsroyale.com/decks/challenge-winners?type=top200&page='
 
     for num in range(1, pagesToParse+1):
         url = top200URL+str(num)
         for deck in get_decks(url):
-            map_cards(deck)
+            link_cards(deck)
 
-    print(cards['Miner'].getMostUsedWith())
+    C = normalize_weights(C)
