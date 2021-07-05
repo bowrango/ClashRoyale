c84c908b2e13d456e9c337633363fb550a356d1e
Matt Bowring <mjb1085@wildcats.unh.edu>
2021-07-04 22:55:22 -0400

    hide key

--- RoyaleAPI/test.py
+++ RoyaleAPI/test.py
@@ -1,8 +1,8 @@
 from client import Client
 
-# key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjkwMDJiMWE3LTRhMTItNDdhMy04MDU2LTU5YmFiZTZmZmVjZiIsImlhdCI6MTYwOTE5Njc3Mywic3ViIjoiZGV2ZWxvcGVyL2MyMGJhNThjLTlkNGUtMDFkZC01YzUwLTI5ZDMzZDZlMDNjNiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMjguMTI4LjEyOC4xMjgiXSwidHlwZSI6ImNsaWVudCJ9XX0.1RKFtdown15NHs9gv32_25jsS2iS7uhrrM0Q2RjRTWNwVAYVwe9Q3FvscLn20DZU9ZhvJ_1vziLATz98JrzShg'
 proxy_url = 'https://proxy.royaleapi.dev/v1'
 
+# save your own key to a key.txt file into the RoyaleAPI directory
 with open('RoyaleAPI/key.txt', 'r') as file:
     dev_key = file.read().replace('\n', '')
 
@@ -13,6 +13,7 @@ players = c.get_top_players(limit=10)
 stats = c.get_all_card_attrs(attribute='cards_stats')
 attrs = c.get_all_card_attrs(attribute='cards')
 print(attrs)
+
 # for player in players.raw_data:
 #     tag = player.raw_data['tag']
 #     player_info = c.get_player(tag)
